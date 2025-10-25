eeb5dde5a7deb68d3ee6cca4dc8dbec074a686b4
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-10-25 13:32:43 +0300

    Implemented login routes, along with authentication via passport

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
@@ -18,4 +19,20 @@ router.post("/register", async (req, res) => {
     res.redirect("/register");
   }
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/campgrounds");
+  }
+);
 module.exports = router;
