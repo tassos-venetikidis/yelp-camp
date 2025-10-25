89528d2dc6d04f949cd9164abb6321b2aabb9e30
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-10-25 18:39:31 +0300

    Amended the register post route to automatically login a newly registered user

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,16 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const user = new User({ username, email });
     const registeredUser = await User.register(user, password);
-    req.flash("success", "Welcome To Yelp Camp!");
-    res.redirect("/campgrounds");
+    req.login(registeredUser, function (err) {
+      if (err) return next(err);
+      req.flash("success", "Welcome To Yelp Camp!");
+      res.redirect("/campgrounds");
+    });
   } catch (e) {
     req.flash("error", e.message);
     res.redirect("/register");
